2921d0d8357d5386824a83e6f83c12dbe111719f
Tomas Kovarik <info@rs-kovarik.cz>
2018-09-18 21:29:26 +0200

    composer edit

--- 01-text/stat.py
+++ 01-text/stat.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+
 import sys
 import re
 from collections import Counter
 
 def composer(file):
 	r = re.compile("Composer: (.*)" )
+	
+	r1 = re.compile("(.*)(\((\+)*\d{4}(.*)\)){1}")
 	counter = Counter()
 	
 	for line in file:
@@ -11,7 +15,11 @@ def composer(file):
 		if m:
 			name = m.group(1).strip()
 			for n in name.split(';'):
-	 			counter[n.strip()] += 1
+				match = r1.match(n);
+				if match:
+	 				counter[match.group(1).strip()] += 1
+				elif (not (n =="")):
+	 				counter[n.strip()] += 1
 	 
 	for k, v in counter.items():
 		print("%s: %d" % (k, v))
