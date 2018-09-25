705d819265b215a3d8748cd9e5d744087ab11279
Tomas Kovarik <info@rs-kovarik.cz>
2018-09-25 22:17:54 +0200

    todo voices + print + return load()

--- 02-object-and-classes/test.py
+++ 02-object-and-classes/test.py
@@ -20,11 +20,12 @@ def load(filename):
     r_title = re.compile("Title: (.*)")
     r_genre = re.compile("Genre: (.*)")
     r_key = re.compile("Key: (.*)")
-    rCompositionYear = re.compile("Composition Year: (.*)")
-    rEdition = re.compile("Edition: (.*)")
-    rEditor = re.compile("Editor: (.*)")
+    r_composition_year = re.compile("Composition Year: (.*)")
+    r_edition = re.compile("Edition: (.*)")
+    r_editor = re.compile("Editor: (.*)")
     rVoice = re.compile("Voice (\d*): (.*)")
-    rPartiture = re.compile("Partiture: (.*)")
+    r_partiture = re.compile("Partiture: (.*)")
+    r_incipit = re.compile("Incipit: (.*)")
 
     prints = []
 
@@ -62,6 +63,38 @@ def load(filename):
             composition.key = key
             continue
 
+        m = r_composition_year.match(line)
+        if m:
+            year = load_composition_year(m)
+            composition.key = year
+            continue
+
+        m = r_partiture.match(line)
+        if m:
+            paritture = load_partiture(m)
+            Print.partiture = paritture
+            continue
+
+        m = r_incipit.match(line)
+        if m:
+            incipit = load_incipit(m)
+            composition.key = incipit
+            continue
+
+        m = r_edition.match(line)
+        if m:
+            edition = load_edition(m)
+            edition_class = Edition(composition, None, edition)
+            continue
+
+        m = r_editor.match(line)
+        if m:
+            authors = load_editor(m)
+            edition_class.authors = authors
+            continue
+
+
+
 
     for print_instance in prints:
         print("Print Number:", print_instance.print_id, print_instance.edition)
@@ -79,7 +112,7 @@ def load_composer(match):
     r_died = re.compile("(.*)(\d{4})(.*)")
     name = match.group(1).strip()
     for n in name.split(';'):
-        match = r1.match(n);
+        match = r1.match(n)
         if match:
             person_name = match.group(1).strip()
             match_borned = r_borned.match(match.group(2).strip())
@@ -118,6 +151,71 @@ def load_key(match):
     key = match.group(1).strip()
     if (not (key == "")):
         return key
+    else:
+        return None
+
+def load_composition_year(match):
+    m = match.group(1).strip()
+    r1 = re.compile("(.*)(\d{4})(.*)")
+    if (not (m == "")):
+        year = r1.match(m)
+        if year:
+            return int(year.group(2).strip())
+    else:
+        return None
+
+
+def load_edition(match):
+    edition = match.group(1).strip()
+    if (not (edition == "")):
+        return edition
+    else:
+        return None
+
+
+def load_editor(match):
+    editors = []
+    r1 = re.compile("(.*)(\((\+)*\d{4}(.*)\)){1}")
+    r_borned = re.compile("\((\+)*(\d{4})(.*)")
+    r_died = re.compile("(.*)(\d{4})(.*)")
+    name = match.group(1).strip()
+    for n in name.split(';'):
+        match = r1.match(n)
+        if match:
+            person_name = match.group(1).strip()
+            match_borned = r_borned.match(match.group(2).strip())
+            if match_borned:
+                borned = match_borned.group(2).strip()
+                match_died = r_died.match(match_borned.group(3).strip())
+                if match_died:
+                    died = match_died.group(2).strip()
+                else:
+                    died = None
+            else:
+                borned = None
+                died = None
+            editors.append(Person(person_name, borned, died))
+        elif (not (n == "")):
+            editors.append(Person(n, None, None))
+
+    return editors
+
+
+def load_partiture(match):
+    partiture = match.group(1).strip()
+    r1 = re.compile("(.*)yes(.*)")
+    if (not (partiture == "")):
+        match = r1.match(partiture)
+        if match:
+            return True
+        return False
+    else:
+        return False
+
+def load_incipit(match):
+    incipit = match.group(1).strip()
+    if (not (incipit == "")):
+        return incipit
     else:
         return None
 
