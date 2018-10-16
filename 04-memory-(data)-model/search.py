dd2f1c1420e38b8ee4fba84cdb324b63fd9fcce7
Tomas Kovarik <info@rs-kovarik.cz>
2018-10-16 23:38:14 +0200

    search.py

--- 04-memory-(data)-model/search.py
+++ 04-memory-(data)-model/search.py
@@ -33,7 +33,7 @@ def main():
         # composer name
         dataForJson[composer[3]] = prints
 
-
+        print(composer)
         composerId = (composer[0], )
         compositionCutsor = conn.cursor()
         compositionCutsor.execute('SELECT * FROM score_author sca \
@@ -41,20 +41,23 @@ def main():
                                WHERE sca.composer = ? ORDER BY sca.composer ASC', composerId)
 
         for composition in compositionCutsor:
-            compositionId = (composition[0],)
+            compositionId = (composition[1],)
 
 
             #pomocny print
             #print("composition:", end=" ")
             #print(composition)
 
-
+            #print (composition)
             editionCursor = conn.cursor()
             editionCursor.execute('SELECT * FROM edition ed \
                                    JOIN score sc on ed.score = sc.id \
                                    WHERE ed.score = ? ORDER BY ed.id ASC', compositionId)
-            for edition in editionCursor:
-                editionId = (edition[0],)
+
+            edition = editionCursor.fetchone()
+            #print (edition)
+            #for edition in editionCursor:
+            editionId = (edition[0],)
 
                 #  #pomocny print
                 #print("Edition:")
@@ -62,41 +65,59 @@ def main():
 
 
 
-                editionAuthorCursor = conn.cursor()
-                editionAuthorCursor.execute('SELECT * FROM edition_author eda \
+            editionAuthorCursor = conn.cursor()
+            editionAuthorCursor.execute('SELECT * FROM edition_author eda \
                                                JOIN person p on eda.editor = p.id \
                                                WHERE eda.edition = ? ORDER BY eda.editor ASC', editionId)
-                editor = ""
-                i = 0
-                editionAuthors = editionAuthorCursor.fetchall()
-                cnt = len(editionAuthors)
-                for editionAuthor in editionAuthors:
+            editors = []
+
+            editionAuthors = editionAuthorCursor.fetchall()
+
+            for editionAuthor in editionAuthors:
+                editor_instance = {}
+
                     #print ("row", cnt)
-                    editor += editionAuthor[6]
-                    if editionAuthor[4] != "NULL":
-                        editor += "(" + str(editionAuthor[4]) + "-"
-                        if editionAuthor[5] != "NULL":
-                            editor += str(editionAuthor[5]) + ")"
-                        else:
-                            editor += " )"
-                    elif editionAuthor[5] != "NULL":
-                         editor += "( -" + str(editionAuthor[5]) + ")"
-
-                    i += 1
-                    if not (cnt == i):
-                        editor += "; "
-
-                    # pomocny print
-                    #print("EditionAutor:", i)
-                    #print(editionAuthor)
+                editor_instance["Name"] = editionAuthor[6]
+                editor_instance["Borned"] = editionAuthor[4]
+                editor_instance["Died"] = editionAuthor[5]
+                editors.append(editor_instance)
+                #if editionAuthor[4] != "NULL":
+                #     editor += "(" + str(editionAuthor[4]) + "-"
+                #     if editionAuthor[5] != "NULL":
+                #        editor += str(editionAuthor[5]) + ")"
+                #    else:
+                #        editor += " )"
+                #                     #elif editionAuthor[5] != "NULL":
+                #     editor += "( -" + str(editionAuthor[5]) + ")"
 
 
 
-                printCursor = conn.cursor()
-                printCursor.execute('SELECT * FROM print pr \
-                                       JOIN edition e on pr.edition = e.id \
-                                       WHERE pr.edition = ? ORDER BY pr.edition ASC', editionId)
-                print_entity = printCursor.fetchone()
+                # pomocny print
+                #print("EditionAutor:", i)
+                #print(editionAuthor)
+
+            compositionAuthorCursor = conn.cursor()
+            compositionAuthorCursor.execute('SELECT * FROM score_author sca \
+                                           JOIN person p on sca.composer = p.id \
+                                           WHERE score = ? ORDER BY sca.composer ASC', (composition[1],))
+
+            composers = []
+
+            composerAutors = compositionAuthorCursor.fetchall()
+
+            for composerAutor in composerAutors:
+                composer_instance = {}
+
+                composer_instance["Name"] = composerAutor[6]
+                composer_instance["Borned"] = composerAutor[4]
+                composer_instance["Died"] = composerAutor[5]
+                composers.append(composer_instance)
+
+            printCursor = conn.cursor()
+            printCursor.execute('SELECT * FROM print pr \
+                                   JOIN edition e on pr.edition = e.id \
+                                   WHERE pr.edition = ? ORDER BY pr.edition ASC', editionId)
+            print_entity = printCursor.fetchone()
                 # pomocny print
                 #print("Print:")
                 #print(print_entity)
@@ -112,14 +133,22 @@ def main():
             print_instance["Title"] = composition[4]
             print_instance["Genre"] = composition[5]
             print_instance["Key"] = composition[6]
+            print_instance["Composers"] = composers
             print_instance["Composition Year"] = composition[8]
 
             print_instance["Publication Year"] = edition[3]
             print_instance["Edition"] = edition[2]
-            print_instance["Editor"] = editor
+            print_instance["Editors"] = editors
 
+            voices = {}
             for voice in voiceCursor:
-                print_instance["Voice "+ str(voice[1])] = voice[3] + "; " + voice[4]
+                voice_tmp = {}
+                voice_tmp["Range"] = voice[3]
+                voice_tmp["Name"] = voice[4]
+                voices[str(voice[1])] =  voice_tmp
+
+            print_instance["Voices"] = voices
+
 
                 #pomocny print
                 #print("Voice:")
@@ -127,6 +156,7 @@ def main():
             print_instance["Partiture"] = print_entity[1]
             print_instance["Incipit"] = composition[7]
             prints.append(print_instance)
+        print ("r")
 
         #print(" ")
 
